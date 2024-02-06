1663067a401ae852c2a630865b7126d389bf67cc
jordan-qr <jordan.x35602@gmail.com>
2024-02-05 20:08:34 -0500

    Cambios generales eliminacion de imagenes y archivos

---
+++ nuestra-empresa/js/script.js
@@ -0,0 +1,57 @@
+function abrirWhatsApp() {
+  window.open('https://api.whatsapp.com/send?phone=936922958', '_blank');
+}
+
+// Función para mostrar u ocultar el ícono de WhatsApp basado en el desplazamiento
+window.onscroll = function() {
+  var iconoWhatsApp = document.querySelector('.whatsapp-icon');
+  var iconoWhatsApp2 = document.querySelector('.whatsapp-icon2');
+  var iconoWhatsApp3 = document.querySelector('.whatsapp-icon3');
+
+  if (document.body.scrollTop >= 20 || document.documentElement.scrollTop >= 20) {
+    iconoWhatsApp.style.display = "block";
+    iconoWhatsApp2.style.display = "block";
+    iconoWhatsApp3.style.display = "block";
+  } else {
+    iconoWhatsApp.style.display = "block";
+    iconoWhatsApp2.style.display = "block";
+    iconoWhatsApp3.style.display = "block";
+  }
+};
+
+document.addEventListener('DOMContentLoaded', function() {
+  var contenedor = document.getElementById('miContenedor');
+
+  // Elimina la clase 'mostrar' si ya existe
+  contenedor.classList.remove('mostrar');
+
+  // Forzar el reflujo (reflow) para reiniciar la animación
+  void contenedor.offsetWidth;
+
+  // Agrega la clase 'mostrar' para iniciar la animación
+  contenedor.classList.add('mostrar');
+});
+document.addEventListener('DOMContentLoaded', function() {
+  var contenedor = document.getElementById('miContenedor2');
+
+  // Elimina la clase 'mostrar' si ya existe
+  contenedor.classList.remove('mostrar');
+
+  // Forzar el reflujo (reflow) para reiniciar la animación
+  void contenedor.offsetWidth;
+
+  // Agrega la clase 'mostrar' para iniciar la animación
+  contenedor.classList.add('mostrar');
+});
+document.addEventListener('DOMContentLoaded', function() {
+  var contenedor = document.getElementById('miContenedor3');
+
+  // Elimina la clase 'mostrar' si ya existe
+  contenedor.classList.remove('mostrar');
+
+  // Forzar el reflujo (reflow) para reiniciar la animación
+  void contenedor.offsetWidth;
+
+  // Agrega la clase 'mostrar' para iniciar la animación
+  contenedor.classList.add('mostrar');
+});
